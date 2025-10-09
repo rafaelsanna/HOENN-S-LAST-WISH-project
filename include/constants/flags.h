34845e8351bbaaf6f093a445e7166f3513e676b8
rafaelsanna <rafaelsanna96#gmail.com>
2025-10-08 22:39:24 -0300

    11.2 quest prologo ajustes nas flags e bugs

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -54,7 +54,7 @@
 #define FLAG_PROLOGUE_COMPLETED    0x270
 #define FLAG_MOSSDEEP_GRANDMA_DIALOGUE    0x271
 #define FLAG_MOSSDEEP_GRANDPA_DIALOGUE    0x272
-#define FLAG_UNUSED_0x02B    0x2B // Unused Flag
+#define FLAG_MOSSDEEP_PROLOGUE_COMPLETED    0x2B
 #define FLAG_UNUSED_0x02C    0x2C // Unused Flag
 #define FLAG_UNUSED_0x02D    0x2D // Unused Flag
 #define FLAG_UNUSED_0x02E    0x2E // Unused Flag
