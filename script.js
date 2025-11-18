1841936f9b71077a7129ea8c945952efb935f3e0
rafaelsanna <rafaelsanna96#gmail.com>
2025-11-18 13:58:33 -0300

    31.4 new new website with BETTER screenshots

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Add scroll effect to header
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 100) {
-        header.style.background = 'rgba(255, 255, 255, 0.98)';
-    } else {
-        header.style.background = 'rgba(255, 255, 255, 0.95)';
-    }
-});
-
 // SIMPLE SLIDESHOW THAT WORKS!
 const slideshowImages = [
     'pokeemerald-0.png',
