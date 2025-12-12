af3c8548db5f89b757ba3f24fc8d479ed1bd5209
rafaelsanna <rafaelsanna96#gmail.com>
2025-12-11 22:59:13 -0300

    44.10 small changes and fix

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -881,12 +881,13 @@
 #define TRAINER_WALLY_MAUVILLE_1            875
 #define TRAINER_GONG                        876
 #define TRAINER_LYLA                        877
+#define TRAINER_JOJI                        878
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      878
+#define TRAINERS_COUNT                      879
 #define MAX_TRAINERS_COUNT                  900
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
