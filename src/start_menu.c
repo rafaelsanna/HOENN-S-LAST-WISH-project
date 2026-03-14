da430ea5ad8c90b49af03c09142a24aa5133c2fb
Pablo Mariante <pablomariante.dm@live.com>
2026-03-14 04:28:07 -0300

    start menu more information

--- src/start_menu.c
+++ src/start_menu.c
@@ -163,7 +163,7 @@ static const struct WindowTemplate sWindowTemplate_StartClock = {
     .tilemapLeft = 1, 
     .tilemapTop = 1, 
     .width = 16,
-    .height = 10,
+    .height = 8,
     .paletteNum = 15,
     .baseBlock = 0x30
 };
@@ -550,7 +550,7 @@ static void Build12HourTimeString(u8 *dest)
 
 static void ShowTimeWindow(void)
 {
-    u8 y = 1;
+    u8 y = 2;
     u8 *ptr;
 
     // print window
@@ -563,19 +563,19 @@ static void ShowTimeWindow(void)
     Build12HourTimeString(gStringVar1);
     StringAppend(gStringVar4, gStringVar1);
     AddTextPrinterParameterized(sStartClockWindowId, FONT_NORMAL, gStringVar4, 0, y, TEXT_SKIP_DRAW, NULL);
-    y += 10;
+    y += 12;
 
     StringCopy(gStringVar4, sText_Name);
     ptr = StringAppend(gStringVar4, gText_Space);
     StringCopy(ptr, gSaveBlock2Ptr->playerName);
     AddTextPrinterParameterized(sStartClockWindowId, FONT_NORMAL, gStringVar4, 0, y, TEXT_SKIP_DRAW, NULL);
-    y += 10;
+    y += 12;
 
     StringCopy(gStringVar4, sText_MoneyPrefix);
     ptr = StringAppend(gStringVar4, gText_EmptyString2);
     ConvertIntToDecimalStringN(ptr, GetMoney(&gSaveBlock1Ptr->money), STR_CONV_MODE_LEFT_ALIGN, 6);
     AddTextPrinterParameterized(sStartClockWindowId, FONT_NORMAL, gStringVar4, 0, y, TEXT_SKIP_DRAW, NULL);
-    y += 10;
+    y += 12;
 
     StringCopy(gStringVar4, sText_LevelCapPrefix);
     ptr = StringAppend(gStringVar4, gText_EmptyString2);
