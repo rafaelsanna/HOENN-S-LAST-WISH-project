9f201726cef130462861984bd0552249c4a31926
rafaelsanna <rafaelsanna96#gmail.com>
2026-03-24 21:54:14 -0300

    now 6 save slots (before it was 4) more space for itens in bag, entering patch 0.4.1

--- include/constants/global.h
+++ include/constants/global.h
@@ -79,11 +79,11 @@
 #define ROAMER_COUNT 1 // Number of maximum concurrent active roamers
 
 // Bag constants
-#define BAG_ITEMS_COUNT 30
-#define BAG_KEYITEMS_COUNT 30
-#define BAG_POKEBALLS_COUNT 16
-#define BAG_TMHM_COUNT 64
-#define BAG_BERRIES_COUNT 46
+#define BAG_ITEMS_COUNT 80
+#define BAG_KEYITEMS_COUNT 60
+#define BAG_POKEBALLS_COUNT 40
+#define BAG_TMHM_COUNT 128
+#define BAG_BERRIES_COUNT 80
 
 // Number of facilities for Ranking Hall.
 // 7 facilities for single mode + tower double mode + tower multi mode.
