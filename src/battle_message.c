fd340a279128847bb5e62b6f2e0dd9abbf2bcd29
rafaelsanna <rafaelsanna96#gmail.com>
2026-03-29 01:32:57 -0300

    53.4 DARK BATTLE UI final version (only need a little small fixing on the YES/NO text

--- src/battle_message.c
+++ src/battle_message.c
@@ -1683,13 +1683,13 @@ static const struct BattleWindowText sTextOnWindowsInfo_Normal[] =
         .shadowColor = 15,
     },
     [B_WIN_YESNO] = {
-    .fillValue = PIXEL_FILL(1),
-    .fontId = FONT_NORMAL,
+    .fillValue = PIXEL_FILL(0xE),
+    .fontId = FONT_NARROW,
     .x = 0,
     .y = 1,
     .speed = 0,
-    .fgColor = 13,
-    .bgColor = 1,
+    .fgColor = 15,
+    .bgColor = 15,
     .shadowColor = 15,
     },
     [B_WIN_LEVEL_UP_BOX] = {
