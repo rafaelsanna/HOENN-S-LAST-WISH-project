13aba0d956250a6564d98deb16a6946d700ffb07
rafaelsanna <rafaelsanna96#gmail.com>
2026-04-02 21:23:22 -0300

    53.20 after pulling ember new map + one trainer at jungle dewdrop

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -893,13 +893,14 @@
 #define TRAINER_SOULLUNA_CASUAL             887
 #define TRAINER_RIO_CASUAL                  888
 #define TRAINER_AURORA                      889
+#define TRAINER_DEW                         890
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      900
-#define MAX_TRAINERS_COUNT                  901
+#define TRAINERS_COUNT                      901
+#define MAX_TRAINERS_COUNT                  902
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
 #endif  // GUARD_CONSTANTS_OPPONENTS_H
