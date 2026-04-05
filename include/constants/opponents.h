95d88800f61150dd774ae904d0528cf7b93979b0
rafaelsanna <rafaelsanna96#gmail.com>
2026-04-05 01:12:03 -0300

    55.1 starters back sprite now are in NDS style (not gen5)

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -902,8 +902,8 @@
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT                      901
-#define MAX_TRAINERS_COUNT                  902
+#define TRAINERS_COUNT                      999
+#define MAX_TRAINERS_COUNT                  1000
 #define TRAINER_PARTNER(partner)           (MAX_TRAINERS_COUNT + partner)
 
 #endif  // GUARD_CONSTANTS_OPPONENTS_H
