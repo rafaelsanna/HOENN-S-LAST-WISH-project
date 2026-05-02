0da7a5e36ccc8050e97ecf71a545a8bedf19cd2b
rafaelsanna <rafaelsanna96#gmail.com>
2026-05-02 19:33:54 -0300

    64.2 lazuli battle

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -918,6 +918,7 @@
 #define TRAINER_AGATHA                      912
 #define TRAINER_LUKA_PETALBURG_GYM          913
 #define TRAINER_HAKARI                      914
+#define TRAINER_LAZULI_1                    915
 
 
 
